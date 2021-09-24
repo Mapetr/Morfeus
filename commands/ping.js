09d6aa67f887968d29f25bfc28a40db7ff8325f9
Petr Mácha <mape.druhy@gmail.com>
2021-09-24 17:15:32 +0200

    first steps in discord.js v13

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ping',
-    description: 'Ping!',
-    cooldown: 1,
-    aliases: ['test'],
-	execute(message, args) {
-		message.channel.send('Pong.');
-	},
-};
--- index.js
+++ index.js
@@ -1,78 +1,35 @@
-require('dotenv').config();
-const Discord = require('discord.js');
 const fs = require('fs');
-const client = new Discord.Client();
+const { Client, Collection, Intents } = require('discord.js');
+const { createCipheriv } = require('crypto');
+require('dotenv').config();
 
-module.exports = {
-  constructor() {
-    this.data = {
-      queue: [],
-      isPlaying: false,
-      nowPlaying: null,
-      dispatcher: null,
-      voiceChannel: null,
-      loopSong: false,
-      loopQueue: false,
-      skipped: false,
-      msgChannel: null,
-      isPlaylist: false,
-      page: null,
-      spotifyDispatcher: false,
-      isSpotify: false,
-    };
-  },
-};
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Discord.Collection();
-client.cooldowns = new Discord.Collection();
-
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-  console.log('Ready!');
-  client.user.setActivity('!', { type: 'WATCHING' });
-});
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
-client.on('message', message => {
-    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot || message.channel === Discord.DMChannel) return;
-    const args = message.content.slice(process.env.PREFIX.length).trim().split(' ');
-    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(interaction.commandName);
 
-    if (message.deletable) message.delete();
-
-    if (!client.commands.has(commandName)) return;
-    const command = client.commands.get(commandName)
-       || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    const { cooldowns } = client;
-
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-        return;
-    }
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    if (!command) return;
 
     try {
-	    command.execute(message, args, client);
+        await command.execute(interaction);
     } catch (error) {
         console.error(error);
+        await interaction.reply({ content: 'Error! :Sadge:', ephemeral: true });
     }
+})
+
+client.once('ready', c => {
+	console.log(`Ready! as ${c.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
-
-
